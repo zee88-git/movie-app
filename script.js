97ea934e07f70c1b011e81547c2a1e6f29c2fcd1
zee <zeeshan.sk.8080@gmail.com>
2020-10-22 18:50:46 +0530

    don't expect it to play movies for you
    ;)

---
+++ script.js
@@ -0,0 +1,80 @@
+//api took from tmdb 
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+                
+// initially y'll get fav movies 
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+//clear func here
+
+
+function showMovies(movies) {
+    // clear main                  
+    main.innerHTML = "";                  
+                                       
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;                         
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+///that movie cards                        
+        movieEl.innerHTML = `
+            <img                   
+                src="${IMGPATH + poster_path}"
+                alt="${title}"              
+            />
+            <div class="movie-info" >
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>                   
+            <div class="overview">
+        
+            <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5.2) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+//search/fetching of movies
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
+
